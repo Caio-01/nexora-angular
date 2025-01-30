66c2949893be3c6217361852384ca786bfa27716
Caio Costa <caiosantos.costa02@gmail.com>
2025-01-30 15:51:11 -0300

    Melhoria no template na Section Sobre

--- src/app/sections/section-sobre/section-sobre.component.ts
+++ src/app/sections/section-sobre/section-sobre.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Icons {
+  svg: string;
+  textIcon: string;
+}
 @Component({
   selector: 'app-section-sobre',
   templateUrl: './section-sobre.component.html',
-  styleUrl: './section-sobre.component.scss'
+  styleUrl: './section-sobre.component.scss',
 })
 export class SectionSobreComponent {
+  constructor() {}
 
+  // Lista de Icons
+  icons: Array<Icons> = [
+    { svg: 'groups', textIcon: 'Equipe Especializada' },
+    { svg: 'engineering', textIcon: 'Soluções Técnicas' },
+    { svg: 'psychology', textIcon: 'Vasta Experiência' },
+    { svg: 'trending_up', textIcon: 'Qualidade e Precisão' },
+  ];
 }
