8da2e9efa44fb96d5ed3b1922a13dc05ca7fd9e4
Caio Costa <caiosantos.costa02@gmail.com>
2025-02-12 22:44:12 -0300

    Atualização no Template da Section Valores

--- src/app/sections/section-valores/section-valores.component.ts
+++ src/app/sections/section-valores/section-valores.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface Valores {
+  icon: string;
+  title: string;
+  description: string;
+}
 @Component({
   selector: 'app-section-valores',
   templateUrl: './section-valores.component.html',
-  styleUrl: './section-valores.component.scss'
+  styleUrl: './section-valores.component.scss',
 })
 export class SectionValoresComponent {
+  constructor() {}
 
+  cardValores: Array<Valores> = [
+    {
+      icon: 'flag',
+      title: 'Objetivo',
+      description:
+        'Entregar projetos eficientes, com qualidade e pontualidade.',
+    },
+    {
+      icon: 'query_stats',
+      title: 'Meta',
+      description:
+        'Ser referência em inovação e sustentabilidade na construção.',
+    },
+    {
+      icon: 'handshake',
+      title: 'Compromisso',
+      description: 'Tecnologia avançada e práticas sustentáveis.',
+    },
+  ];
 }
