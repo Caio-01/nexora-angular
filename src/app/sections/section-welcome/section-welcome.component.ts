3835ad825beb08265f1ccf8f15ca4d4781810fd6
Caio Costa <caiosantos.costa02@gmail.com>
2025-04-15 20:48:52 -0300

    Adicionado o GSAP ao projeto e aplicado na section Welcome

--- src/app/sections/section-welcome/section-welcome.component.ts
+++ src/app/sections/section-welcome/section-welcome.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-section-welcome',
   templateUrl: './section-welcome.component.html',
-  styleUrl: './section-welcome.component.scss'
+  styleUrl: './section-welcome.component.scss',
 })
-export class SectionWelcomeComponent {
+export class SectionWelcomeComponent implements AfterViewInit {
+  constructor() {}
 
+  // Vai acessar o elementos do DOM com base na referencia
+  @ViewChild('textWelcome') textWelcome!: ElementRef;
+
+  // Chamado após o template renderizar completamente o view do componente
+  ngAfterViewInit(): void {
+    gsap.fromTo(
+      this.textWelcome.nativeElement,
+      {
+        opacity: 0,
+        y: '10%',
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: 'power4.out',
+      }
+    );
+  }
 }
